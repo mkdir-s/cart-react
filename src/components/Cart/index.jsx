fe82e10083fe3b5ecfa99a5eb9e2d460d03da78d
mkdir-s <artyom.alexeyenko@gmail.com>
2023-08-01 22:39:24 +0600

    feat: add data.js, render data into components

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
+
 import CartFooter from "../CartFooter";
 import CartHeader from "../CartHeader";
 import Product from "../Product";
 
+import data from './../../data';
+
 const Cart = () => {
+
+  const [cart, setCart] = useState(data);
+
+  const products = cart.map((product) => {
+    return <Product product={product} key={product.id} />
+  })
+
   return (
     <section className="cart">
       <CartHeader />
 
-      <Product />
+      {products}
 
       <CartFooter />
     </section>
--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -1,24 +1,27 @@
-import './style.scss';
+import "./style.scss";
 
-import Count from '../Count';
-import ButtonDelete from '../ButtonDelete';
+import Count from "../Count";
+import ButtonDelete from "../ButtonDelete";
 
-const Product = () => {
+const Product = ({product}) => {
+
+  const {img, title, price, count, id} = product;
+
   return (
-<section className="product">
-        <div className="product__img">
-          <img src="./img/products/macbook.jpg" alt="Apple MacBook Air 13" />
-        </div>
-        <div className="product__title">Apple MacBook Air 13</div>
-        <div className="product__count">
-          <Count />
-        </div>
-        <div className="product__price">110 000 руб.</div>
-        <div className="product__controls">
-          <ButtonDelete />
-        </div>
-      </section>
-  )
-}
+    <section className="product">
+      <div className="product__img">
+        <img src={`./img/products/${img}`} alt="Apple MacBook Air 13" />
+      </div>
+      <div className="product__title">{title}</div>
+      <div className="product__count">
+        {/* <Count /> */}
+      </div>
+      <div className="product__price">{price} руб.</div>
+      <div className="product__controls">
+        <ButtonDelete />
+      </div>
+    </section>
+  );
+};
 
 export default Product;
